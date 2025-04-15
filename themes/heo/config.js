acd6492e66bedb55931fc482c9e70a2f22beaec3
94-Alex <563627739@qq.com>
2025-04-15 14:11:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -119,7 +119,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://www.yang94.com',
+  HEO_SOCIAL_CARD_URL: '/images/heo/13-removebg-preview.webp',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
